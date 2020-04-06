90c750a3b5cff1b5446f60b93034abf30dbffa8f
feleventh <feleventh@hotmail.com>
2020-04-06 21:18:07 +0800

    add modify

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.cookie('city', 'guangzhou', {
-    SameSite: "none",
-    Secure: true
-  })
-  res.render('index', { title: '测试cookie' });
+  res.setHeader('Set-Cookie', 'city=shenzhen;domain=.xiangyouzou.xyz;SameSite=none;secure=true')
+  res.render('index', { title: '测试cookie，使用setHead' });
 });
 router.all('/test', function(req, res, next) {
-  console.log(req.cookies)
-  res.render('test', { title: '测试' });
+  res.cookie('name', 'zhangsan', {
+    sameSite: "none",
+    secure: true,
+  })
+  res.render('test', { title: 'cookie测试,使用cookie方法' });
 });
 
 module.exports = router;
