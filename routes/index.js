0a53d5cb1634aa849531c3d6ab2d5debf562b39d
feleventh <feleventh@hotmail.com>
2020-04-07 22:32:33 +0800

    modify post

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,21 @@ router.get('/', function(req, res, next) {
   res.setHeader('Set-Cookie', 'city=shenzhen;domain=test.xiangyouzou.xyz;SameSite=none;secure=true')
   res.render('index', { title: '测试cookie，使用setHead' });
 });
-router.all('/test', function(req, res, next) {
+router.post('/', function(req, res, next) {
+  res.setHeader('Set-Cookie', 'city=hassamesite;domain=test.xiangyouzou.xyz;SameSite=none;secure=true')
+  res.json({name: 'lisi', method: 'post'})
+});
+router.get('/test', function(req, res, next) {
   res.cookie('name', 'zhangsan', {
     sameSite: 'none',
     secure: true,
     domain: 'test.xiangyouzou.xyz'
   })
   res.render('test', { title: 'cookie测试,使用cookie方法' });
+});
+router.post('/test', function(req, res, next) {
+  res.setHeader('Set-Cookie', 'city=nosetsamesite;domain=test.xiangyouzou.xyz;')
+  res.json({name: 'lisi', method: 'post'})
 });
 router.all('/test1', function(req, res, next) {
   res.cookie('weather', 'sunny', {
